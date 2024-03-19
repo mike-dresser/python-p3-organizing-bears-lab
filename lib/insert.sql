b3a57566563788a577c6980b12744b80c47e84b7
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-03-19 13:01:15 -0400

    complete

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears
+(name, age, sex, color, temperament, alive)
+VALUES 
+    ('Mr. Chocolate', 3, 'M','brown', 'sleepy', 1),
+    ('Rowdy', 4, 'M', 'black', 'rowdy', 1),
+    ('Tabitha', 4, 'F','black', 'sleepy', 1),
+    ('Seargeant Brown', 4, 'M','brown', 'militant', 0),
+    ('Melissa', 2, 'F','beige', 'sweet', 0),
+    ('Grinch', 2, 'M','green', 'holly jolly', 1),
+    ('Wendy', 2, 'F','red', 'beefy', 1),
+    (NULL, 3, 'F','transparent', 'mysterious', 1);
